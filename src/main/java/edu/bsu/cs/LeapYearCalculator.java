c882620559d41edae0136ed18f937107b8df053e
Toni <tonibenson17@gmail.com>
2024-09-18 17:03:47 -0400

    intial push

--- src/main/java/edu/bsu/cs/LeapYearCalculator.java
+++
@@ -1,9 +0,0 @@
-package edu.bsu.cs;
-
-
-public class LeapYearCalculator {
-    public boolean isLeapYear(int year){
-        return ((year%4 == 0 && year%100 !=0)) || (year%400 == 0 && year%100 == 0);
-
-    }
-}
